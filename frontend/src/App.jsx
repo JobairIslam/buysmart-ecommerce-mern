eb5a8d5bee62e72a1c5e0db49c4b928251f61a3a
JobairIslam <jubairislamasif@gmail.com>
2025-06-03 16:16:18 +0600

    setup the route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Product from "./pages/Product"
+import PlaceOrder from "./pages/PlaceOrder"
+import Orders from "./pages/Orders"
+import Login from "./pages/Login"
+import Contact from "./pages/Contact"
+import Collection from "./pages/Collection"
+import Cart from "./pages/Cart"
 
 const App = () => {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/product' element={<Product/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+      </Routes>
+    </div>
   )
 }
 
