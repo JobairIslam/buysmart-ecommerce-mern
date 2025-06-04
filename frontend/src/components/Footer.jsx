10f6248e7f563bb81053e193035fda827ec23736
JobairIslam <jubairislamasif@gmail.com>
2025-06-04 21:27:27 +0600

    checking the vercel

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="w-full bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div>Footer ibrahim abir</div>
+        <div>Footer checking the vercel</div>
       </div>
     </div>
   )
