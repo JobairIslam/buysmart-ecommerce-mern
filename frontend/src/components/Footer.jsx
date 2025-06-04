25950cf1009acda8bb8371e117a84f8ca80c5116
JobairIslam <jubairislamasif@gmail.com>
2025-06-04 20:46:30 +0600

    initial commit2

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="w-full bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div>Footer jubair islam asif</div>
+        <div>Footer ibrahim abir</div>
       </div>
     </div>
   )
